14c9f2af24671edb763d03adb05b1dcddc97265e
sahal-ahmed <sahal.cox@gmail.com>
2022-12-05 19:30:17 +0600

    10ths commit

--- hajjinfo/Hotel/forms.py
+++ hajjinfo/Hotel/forms.py
@@ -4,10 +4,16 @@ from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 class OwnerRegistrationForm(forms.ModelForm):
-    contact = PhoneNumberField(
-        widget=PhoneNumberPrefixWidget(initial="Bn")
-    )
-    password = forms.CharField(widget=forms.PasswordInput)
+    
+    
     class Meta:
         model = Owner
         fields = ('first_name','last_name','email','contact','hotel_name','password')
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'last_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'email' : forms.EmailInput(attrs={'class':'form-control'}),
+            'contact' : forms.NumberInput(attrs={'class':'form-control','placeholder':'e.g. +12125552368'}),
+            'hotel_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'password' : forms.PasswordInput(attrs={'class':'form-control'}),
+        }
--- hajjinfo/Hotel/models.py
+++ hajjinfo/Hotel/models.py
@@ -3,7 +3,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 import uuid
 # Create your models here.
 class Owner(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=10)
     first_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=254)
